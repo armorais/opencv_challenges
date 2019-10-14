cfc9a187da02d14ed3e85156331bc0414f9c1e64
Alison Morais <armorais01@gmail.com>
2019-10-14 13:50:02 -0300

    Atulizacao do vignette

---
+++ aula_2/build/CMakeFiles/color_segmentation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/color_segmentation.dir/src/color_segmentation.o"
+  "color_segmentation.pdb"
+  "color_segmentation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/color_segmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ aula_2/build/CMakeFiles/vignette.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vignette.dir/src/vignette.o"
+  "vignette.pdb"
+  "vignette"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vignette.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
