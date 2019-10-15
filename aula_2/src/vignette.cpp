65fb08aca4b4866551dcd493fc8b2bcf04a766de
Alison Morais <armorais01@gmail.com>
2019-10-15 14:51:38 -0300

    Corrigido vignette para funcionar com qualquer imagem

--- aula_2/src/vignette.cpp
+++ aula_2/src/vignette.cpp
@@ -43,12 +43,14 @@ int main(int argc, char **argv)
 {
     string path = argv[1];
     double sigma = stoi(argv[2]);
-    double x = stoi(argv[3]);
-    double y = stoi(argv[4]);
+    int x = stoi(argv[3]);
+    int y = stoi(argv[4]);
 
     Mat image = imread(path);
-    resize(image,image,Size(), 0.5,0.5);
     Mat kernel_x, kernel_y, kernel, mask, output;
+    Rect roi;
+    
+    resize(image,image,Size(), 0.8,0.8);
 
     // getGaussianKernel recieves (aperture size, gaussian standard deviation, type of filter coefficients)
     kernel_x = getGaussianKernel(int(2.5*image.cols), sigma, CV_32F);
@@ -58,10 +60,21 @@ int main(int argc, char **argv)
     
     mask = 255 * kernel / norm(kernel);
 
-    int x_pos = -(5 * x) + 600;
-    int y_pos = ((41/10) * y) + 40;
+    int offset_x = int(image.cols)/2;
+    int offset_y = int(image.rows)/2;
 
-    mask = mask(Range(y_pos,int(image.rows)+y_pos),Range(x_pos,int(image.cols)+x_pos));
+    x = (-2*x)+200;
+    y = 2*y;
+
+    double a = ((x - 50.0)/100.0);
+    double b = ((y - 50.0)/100.0);
+    roi.x = int(offset_x + (offset_x * a));
+    roi.y = int(offset_y + (offset_y * b));
+
+    roi.width = image.cols;
+    roi.height = image.rows;
+
+    mask = mask(roi);
 
     mask.convertTo(mask, CV_8U, 255);
     cvtColor(mask, mask, COLOR_GRAY2RGB);
