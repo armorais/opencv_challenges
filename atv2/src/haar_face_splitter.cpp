43707626d321da468c0ef197f7db4cf5868ae02d
alison <alison@core>
2019-10-21 18:17:01 -0300

    Adicionado reconhecimento de face e olho diretiro

--- atv2/src/haar_face_splitter.cpp
+++ atv2/src/haar_face_splitter.cpp
@@ -10,9 +10,13 @@ void detectAndDisplay( Mat frame );
 
 /** Global variables */
 String left_eye_cascade_name = "../haar_data/haarcascade_mcs_lefteye.xml";
+String right_eye_cascade_name = "../haar_data/haarcascade_mcs_righteye.xml";
 String mouth_cascade_name = "../haar_data/haarcascade_mcs_mouth.xml";
+String face_cascade_name = "../haar_data/haar_face.xml";
 CascadeClassifier left_eye_cascade;
+CascadeClassifier right_eye_cascade;
 CascadeClassifier mouth_cascade;
+CascadeClassifier face_cascade;
 string window_name = "Capture - Face detection";
 RNG rng(12345);
 
@@ -23,11 +27,13 @@ int main( int argc, const char** argv ) {
   // Read
   Mat image = imread(path, cv::IMREAD_UNCHANGED);
 
-  resize(image,image,Size(),0.5,0.5);
+  //resize(image,image,Size(),0.5,0.5);
 
   // Load the cascades
-  if( !left_eye_cascade.load( left_eye_cascade_name ) ){ printf("--(!)Error loading eye\n"); return -1; };
-  if( !mouth_cascade.load( mouth_cascade_name ) ){ printf("--(!)Error loading eye\n"); return -1; };
+  if( !left_eye_cascade.load( left_eye_cascade_name ) ){ printf("--(!)Error loading left eye\n"); return -1; };
+  if( !right_eye_cascade.load( right_eye_cascade_name ) ){ printf("--(!)Error loading right eye\n"); return -1; };
+  if( !mouth_cascade.load( mouth_cascade_name ) ){ printf("--(!)Error loading mouth\n"); return -1; };
+  if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face\n"); return -1; };
 
   // Calls the detection function
   detectAndDisplay( image );
@@ -36,9 +42,10 @@ int main( int argc, const char** argv ) {
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat image ) {
-  std::vector<Rect> eyes;
+  std::vector<Rect> left_eyes;
+  std::vector<Rect> right_eyes;
   std::vector<Rect> mouths;
-  Rect face;
+  std::vector<Rect> faces;
   Mat image_gray;
   
   // Converts to grayscale
@@ -47,17 +54,41 @@ void detectAndDisplay( Mat image ) {
   // Equalizes the histogram to normalize brightness and contrast
   equalizeHist( image_gray, image_gray );
 
-  // Detects mouth
-  mouth_cascade.detectMultiScale( image_gray, mouths, 1.1, 100, 1|CASCADE_FIND_BIGGEST_OBJECT, Size(30, 30) );
+  // Detects face
+  face_cascade.detectMultiScale( image_gray, faces, 1.1, 2, 0, Size(60, 60) );
 
+  for( size_t i = 0; i < faces.size(); i++ ) {
+        rectangle(image, faces[i].tl(), faces[i].br(), Scalar( 255, 0, 0 ), 2);
 
-  for( size_t i = 0; i < mouths.size(); i++ ) {
-    Point center( mouths[i].x + mouths[i].width*0.5, mouths[i].y + mouths[i].height*0.5 );
-    ellipse( image, center, Size( mouths[i].width*0.5, mouths[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        Mat faceROI = image( faces[0] );
+        
+        // Detects mouth
+        mouth_cascade.detectMultiScale( faceROI, mouths, 1.4, 6, 0, Size(10, 10) );
 
-    Mat faceROI = image_gray( mouths[i] );
+        for( size_t j = 0; j < mouths.size(); j++ ) {
+              rectangle(faceROI, mouths[j].tl(), mouths[j].br(), Scalar( 255, 0, 255 ), 2);
+              break;
+        }
+
+        // Detects left eye
+        left_eye_cascade.detectMultiScale( faceROI, left_eyes, 1.3, 6, 0, Size(10, 10) );
+
+        for( size_t j = 0; j < left_eyes.size(); j++ ) {
+              rectangle(faceROI, left_eyes[j].tl(), left_eyes[j].br(), Scalar( 0, 0, 255 ), 2);
+              break;
+        }
+
+        // Detects right eye
+        right_eye_cascade.detectMultiScale( faceROI, right_eyes, 1.4, 6, 0, Size(10, 10) );
+
+        for( size_t j = 0; j < right_eyes.size(); j++ ) {
+              rectangle(faceROI, right_eyes[j].tl(), right_eyes[j].br(), Scalar( 0, 0, 0 ), 2);
+              break;
+        }
+
+        break;
   }
-  // Show the results
+  resize(image, image, Size() ,0.5, 0.5);
   imshow( window_name, image );
   waitKey(0);
 }
