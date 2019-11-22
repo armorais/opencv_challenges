7347668cdd00aff6f2d9a696cd1c40f7cf001d1f
Alison Morais <armorais01@gmail.com>
2019-11-22 17:35:41 -0300

    Modificado script de instalacao no rpi para instalacao global

--- opencv_install_raspberry_pi.sh
+++ opencv_install_raspberry_pi.sh
@@ -87,7 +87,7 @@ mkdir build
 cd build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-            -D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
+            -D CMAKE_INSTALL_PREFIX=/usr/local \
             -D INSTALL_C_EXAMPLES=ON \
             -D INSTALL_PYTHON_EXAMPLES=ON \
             -D WITH_TBB=ON \
